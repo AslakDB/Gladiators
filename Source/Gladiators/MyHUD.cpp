2ee015dcfc0ccd47ba7a5addd2767e6c279fa827
AslakDB <Askelakken@gmail.com>
2023-04-10 11:08:29 +0200

    first commit

---
+++ Source/Gladiators/MyHUD.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyHUD.h"
+#include "EnhancedInput/Public/InputMappingContext.h"
+#include "EnhancedInput/Public/EnhancedInputSubsystems.h"
+
+void AMyHUD::GetPlayerHealth()
+{
+	
+}
+
+void AMyHUD::GetScore()
+{
+
+}
+
+void AMyHUD::GetItemDescription()
+{
+
+}
---
+++ Source/Gladiators/PlayerUserWidget.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATORS_API UPlayerUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+		UFUNCTION(BlueprintCallable, Category = "HUD")
+		void SetPlayerHealth(float CurrentHealth, float MaxHealth);
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+		class UProgressBar* HealthBar;
+};
