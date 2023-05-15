03d6b49a759cebfd7ecccdbf9a9aca6a7b304029
Hannabk01 <hannabk01@gmail.com>
2023-05-15 13:22:36 +0200

    Hit and stuff

--- Source/Gladiators/MySweetBabyBoi.h
+++ Source/Gladiators/MySweetBabyBoi.h
@@ -28,6 +28,7 @@ public:
 	AMySweetBabyBoi();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UFUNCTION(BlueprintCallable)
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 	virtual void GetHit_Implementation(const FVector& ImpactPoint, AActor* ActorHit) override;
 
